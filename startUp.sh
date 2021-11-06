375b18d6e0ff8f6690c2fb6025a2b62521f1f136
IgaliMarcell <90566872+IgaliMarcell@users.noreply.github.com>
2021-11-06 12:57:07 +0000

    Update startUp.sh

--- startUp.sh
+++ startUp.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
+##
+##language
+##
 sudo apt-get install x11-xkb-utils
-
 read -p "Default language?(hu)[y/n]>" lang
 if [ $lang = "y" ]
 then
@@ -12,10 +14,14 @@ read -p "What will be the language?>" Clang
 echo "Changing to the choosen language...$Clang"
 setxkbmap $Clang
 fi
-
+##
+##tree
+##
 echo "Downloading tree..."
 sudo apt install tree
-
+##
+##SSH
+##
 read -p "Start ssh service?[y/n]>" Sssh
 if [ $Sssh = "y" ]
 then
@@ -24,17 +30,25 @@ sudo service ssh start
 echo "SSH service analysis..."
 sudo service ssh status
 fi
-
+##
+##vscode
+##
 read -p "Install vscode?[y/n]>" vscode
 if [ $vscode = "y" ]
 then
 echo "Downloading vscode..."
 sudo wget https://www.dropbox.com/s/dlqb6dw6d920fzp/vscode.deb?dl=0
-sudo mv /home/$USER/vscode /home/$USER/Downloads
-echo "Download completed"
+sudo mv /home/$USER/Desktop/vscode.deb?dl=0 /home/$USER/Downloads
+echo "Download completed" 
 echo "Starting install..."
-dpkg -i /home/$USER/Downloads/vscode.deb
+sudo dpkg -i /home/$USER/Downloads/vscode.deb?dl=0
 echo "Installing finished!"
+fi
+read -p "Remove vscode package?[y/n]>" rmVscode 
+if [ $rmVscode = "y" ]
+then 
+sudo rm /home/$USER/Downloads/vscode.deb?dl=0
+echo "Removing the package was completed!"
 fi
 
 
